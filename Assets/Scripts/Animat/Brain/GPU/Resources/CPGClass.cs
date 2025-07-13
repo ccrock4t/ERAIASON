9474cefece240b7d7031abbb3d47d35c316cd1cf
Rohan Jay <rjay2@jh.edu>
2025-07-13 17:42:37 -0400

    Squashed commit of the following:
    commit d781dad2bc9131e42f5d0194e5f8abfaa8305c46
    Author: Rohan Jay <75583911+roha913@users.noreply.github.com>
    Date:   Thu Jul 10 20:32:47 2025 -0400
    
        Include r, w, p in Neuron struct
    
    commit b5fc7d9790da08b19098c6db663086e01e067e6a
    Author: Rohan Jay <75583911+roha913@users.noreply.github.com>
    Date:   Wed Jul 9 14:59:16 2025 -0400
    
        Update CPGClass.cs
    
    commit 1ac93a5b4f6f9b924e326e1bba4859622d41a0ac
    Author: Rohan Jay <75583911+roha913@users.noreply.github.com>
    Date:   Wed Jul 9 14:20:20 2025 -0400
    
        Create CPGClass.cs

--- Assets/Scripts/Animat/Brain/GPU/Resources/CPGClass.cs
+++ Assets/Scripts/Animat/Brain/GPU/Resources/CPGClass.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using static Brain;
 public class CPG{
   public Neuron[] neuron_set;
   public float[] time_constants;
@@ -9,61 +7,33 @@ public class CPG{
   public float[] r_s;
   public int n;
   public CPG(Neuron[] neuron_set, float[] time_constants, float[] w_s, float[] p_s, float[] r_s){
-    if(neuron_set.Length % 4 != 0){
-      throw new Exception("# of neurons needs to be a multiple of 4");
-    }
-    for(int i = 0; i < neuron_set.Length; i++){
-      if(neuron_set[i].neuron_role != Neuron.NeuronRole.Motor){
-        throw new Exception("needs to be all motor neurons");
+    for(int i = 0; i < neuron_set.length; i++){
+      if(neuron_set[i].neuron_role != 2){
+        throw new Exception("Ahhhhhhhhhhhhhhhhhhhh!!!!!!!!!!!!!!!!!!");//TODO: Come up with a better exception message
       }
     }
-    this.neuron_set = neuron_set;
-    this.time_constants = time_constants;
-    this.w_s = w_s;
-    this.p_s = p_s;
-    this.r_s = r_s;
-    n = neuron_set.Length;
+    this->neuron_set = neuron_set;
+    this->time_constants = time_constants;
+    this->w_s = w_s;
+    this->p_s = p_s;
+    this->r_s = r_s;
+    n = neuron_set.length;
   }
   private float Sigmoid(float x){
-    return (float)(1.0 /(1.0 + Math.Exp(-x)));
+    return 1.0/(1.0 + Math.Exp(-x));
   }
   public void updateActivations(){
     for(int i = 0; i < n; i++){
-      neuron_set[i].activation = (float)(r_s[i]*Math.Sin(w_s[i]*(Time.time - p_s[i])) + (1 - r_s[i])*Sigmoid(neuron_set[i].activation));
+      neuron_set[i].activation = r_s[i]*Math.Sin(w_s[i]*(Time.Time - p_s[i])) + (1 - r_s[i])*Sigmoid(neuron_set[i].activation);
     }
   }
-  public float[] fitnessFunction(){
-    int num_per_limb = neuron_set.Length/4;
-    float[] fitnessPerLimb = new float[4];
-    fitnessPerLimb[0] = 0; fitnessPerLimb[1] = 0; fitnessPerLimb[2] = 0; fitnessPerLimb[3] = 0;
-    float min_act = 1000000000;
-    for(int i = 0; i < num_per_limb; i++){
-      if(neuron_set[i].activation < min_act){
-        min_act = neuron_set[i].activation;
-      }
+  public virtual float fitnessFunction() = 0;
+  public float simulate(int num_iters){
+    float sum = 0f;
+    for(int iter = 0; iter < num_iters; iter++){
+      updateActivations();
+      sum += fitnessFunction();
     }
-    fitnessPerLimb[0] = min_act;
-    min_act = 1000000000;
-    for(int i = num_per_limb; i < 2*num_per_limb; i++){
-      if(neuron_set[i].activation < min_act){
-        min_act = neuron_set[i].activation;
-      }
-    }
-    fitnessPerLimb[1] = min_act;
-    min_act = 1000000000;
-    for(int i = 2*num_per_limb; i < 3*num_per_limb; i++){
-      if(neuron_set[i].activation < min_act){
-        min_act = neuron_set[i].activation;
-      }
-    }
-    fitnessPerLimb[2] = min_act;
-    min_act = 1000000000;
-    for(int i = 3*num_per_limb; i < neuron_set.Length; i++){
-      if(neuron_set[i].activation < min_act){
-        min_act = neuron_set[i].activation;
-      }
-    }
-    fitnessPerLimb[3] = min_act;
-    return fitnessPerLimb;
+    return sum;
   }
 }
