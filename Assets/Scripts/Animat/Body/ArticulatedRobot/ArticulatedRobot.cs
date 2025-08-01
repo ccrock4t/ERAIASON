89439a5357ba89065fc4f80b97546a464306e48c
ccrock4t <15344554+ccrock4t@users.noreply.github.com>
2025-08-01 18:41:15 -0400

    Revert "restore head position as node0"
    This reverts commit bae5273e90197a22fcde63046ba1857635c85140.

--- Assets/Scripts/Animat/Body/ArticulatedRobot/ArticulatedRobot.cs
+++ Assets/Scripts/Animat/Body/ArticulatedRobot/ArticulatedRobot.cs
@@ -72,9 +72,6 @@ public class ArticulatedRobot : AnimatBody
         this.CreateGenomeAndInstantiateBody(body_genome);
         this.root_ab = this.root_gameobject.GetComponent<ArticulationBody>();
 
-        this.vision_segment = root_gameobject;
-        
-
         // ignore self collisions
         Collider[] colliders = GetComponentsInChildren<Collider>();
 
@@ -556,6 +553,11 @@ public class ArticulatedRobot : AnimatBody
 
         Transform segment = nodeGO.transform.GetChild(0).GetChild(0).Find("Segment");
 
+        if (n.name.Contains("head"))
+        {
+            this.vision_segment = segment.gameObject;
+        }
+
         ArticulationBody xDrive_ab, yDrive_ab;
         ArticulationBody zDrive_ab = nodeGO.GetComponent<ArticulationBody>();
         ArticulationJointType joint_type;
@@ -773,7 +775,7 @@ public class ArticulatedRobot : AnimatBody
     public override (Vector3, Vector3) GetVisionSensorPositionAndDirection()
     {
         var vision_transform = GetVisionSensorSegment().transform;
-        return (vision_transform.position, -vision_transform.up);
+        return (vision_transform.position + 0.5f*vision_transform.up, vision_transform.up);
     }
 
     public override Vector3 GetVisionSensorUpDirection()
@@ -781,5 +783,4 @@ public class ArticulatedRobot : AnimatBody
         var vision_transform = GetVisionSensorSegment().transform;
         return vision_transform.forward;
     }
-
 }
